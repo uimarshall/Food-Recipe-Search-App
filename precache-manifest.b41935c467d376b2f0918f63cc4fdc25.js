862aeea26d6769305a642645a75f8d51416f89fe
uimarshall <uimarshall@gmail.com>
2020-04-10 13:07:55 +0100

    Updates

---
+++ precache-manifest.b41935c467d376b2f0918f63cc4fdc25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66550e191ea15b68d1a55d8233d58ee4",
+    "url": "/Food-Recipe-Search-App/index.html"
+  },
+  {
+    "revision": "d79d574dacee34e10bad",
+    "url": "/Food-Recipe-Search-App/static/css/main.dafc78eb.chunk.css"
+  },
+  {
+    "revision": "13029ad88e23d3f4db9c",
+    "url": "/Food-Recipe-Search-App/static/js/2.4cd21184.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Recipe-Search-App/static/js/2.4cd21184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79d574dacee34e10bad",
+    "url": "/Food-Recipe-Search-App/static/js/main.4403076c.chunk.js"
+  },
+  {
+    "revision": "77952b47f1d8e3e75571",
+    "url": "/Food-Recipe-Search-App/static/js/runtime-main.aa3db20d.js"
+  },
+  {
+    "revision": "83c058d0843dfbaefba05a7719395405",
+    "url": "/Food-Recipe-Search-App/static/media/background-3.83c058d0.jpeg"
+  }
+]);
